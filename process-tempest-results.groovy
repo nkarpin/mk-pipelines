1733f7ebbba1585f5ad7fc661a329ecb71502ac7
Mykyta Karpin <mkarpin@mirantis.com>
2017-08-09 14:17:52 +0300

    [WIP] Openstack component check/gate pipelines
    Change-Id: I24be70d9234133dd1cb8c08d74deed3bd562f7ed
    Related-Prod: OSCORE-270

---
+++ process-tempest-results.groovy
@@ -0,0 +1,80 @@
+/**
+ *
+ * Tempest results processing pipeline
+ *
+ * Expected parameters:
+ *   TARGET_JOB                   Name of the testing job
+ *   TARGET_BUILD_NUMBER          Number of the testing build
+ *   TEST_REPORTER_IMAGE          Docker image for testrail reporter
+ *   TESTRAIL                     Whether upload results to testrail or not
+ *   TEST_MILESTONE               Product version for tests
+ *   TEST_MODEL                   Salt model used in environment
+ *   OPENSTACK_VERSION            Version of openstack which was tested
+ *   TEST_SUITE                   Testrail test suite
+ *   TEST_PLAN                    Testrail test plan
+ *   TEST_GROUP                   Testrail test group
+ *   TESTRAIL_QA_CREDENTIALS      Credentials for upload to testrail
+ *   TEST_DATE                    Date of test run
+ *
+ */
+
+common = new com.mirantis.mk.Common()
+test = new com.mirantis.mk.Test()
+
+node("python") {
+    try {
+        testOutputDir = "output-${TARGET_JOB}-${TARGET_BUILD_NUMBER}"
+        sh("mkdir -p ${testOutputDir}")
+
+        stage('Get tests artifacts') {
+            selector = [$class: 'SpecificBuildSelector', buildNumber: "${TARGET_BUILD_NUMBER}"];
+
+            step ([$class: 'CopyArtifact',
+                   projectName: TARGET_JOB,
+                   selector: selector,
+                   filter: '_artifacts/rally_reports.tar',
+                   target: testOutputDir,
+                   flatten: true]);
+
+            dir(testOutputDir) {
+                sh("tar -xf rally_reports.tar")
+            }
+        }
+
+        report = sh(script: "find ${testOutputDir} -name *.xml", returnStdout: true).trim()
+        reportPath = "${WORKSPACE}/${report}"
+
+        if (TESTRAIL.toBoolean() == true) {
+            stage('Upload tests results to Testrail'){
+
+                def plan = TEST_PLAN ?: "${MILESTONE}-${TEST_MODEL}-${OPENSTACK_VERSION}-${TEST_DATE}"
+                def group = TEST_GROUP ?: "${TEST_MODEL}-${TEST_SUITE}"
+
+                test.uploadResultsTestrail(reportPath, TEST_REPORTER_IMAGE, group, TESTRAIL_QA_CREDENTIALS,
+                    plan, TEST_MILESTONE, TEST_SUITE)
+            }
+        }
+
+        stage('Check tests results'){
+            fileContents = new File(reportPath)
+            parsed = new XmlParser().parse(fileContents)
+            res = parsed['testsuite'][0].attributes()
+            failures = res.failures.toInteger()
+
+            test_info = """
+                        Failed:  ${failures}
+                        Errors:  ${res.errors}
+                        Skipped: ${res.skipped}
+                        Tests:   ${res.tests}
+                        """
+            println test_info
+
+            if (failures > 0){
+                error("${failures} TEMPEST TESTS HAVE FAILED")
+            }
+        }
+    } catch (Throwable e) {
+        currentBuild.result = 'FAILURE'
+        throw e
+    }
+}
--- test-openstack-component-pipeline.groovy
+++ test-openstack-component-pipeline.groovy
@@ -4,25 +4,116 @@
 
  * Flow parameters:
  *   CREDENTIALS_ID
+ *   COMPONENT
  *   EXTRA_FORMULAS
  *   FORMULAS_REVISION
  *   FORMULAS_SOURCE
+ *   GERRIT_CHECK
+ *_  MILESTONE
+ *   OPENSTACK_VERSION
+ *   OPENSTACK_API_URL
+ *   OPENSTACK_API_CREDENTIALS
+ *   OPENSTACK_API_PROJECT
+ *   OPENSTACK_API_PROJECT_DOMAIN
+ *   OPENSTACK_API_PROJECT_ID
+ *   OPENSTACK_API_USER_DOMAIN
+ *   OPENSTACK_API_CLIENT
+ *   OPENSTACK_API_VERSION
+ *   SALT_MASTER_CREDENTIALS
+ *   SALT_MASTER_URL
+ *   SALT_OVERRIDES
  *   SALT_OPTS
  *   STACK_DEPLOY_JOB
+ *   STACK_DELETE
+ *   STACK_TEST_JOB
+ *   STACK_TYPE
+ *   TESTRAIL
+ *   TEST_TEMPEST_TARGET
+ *   TEST_TEMPEST_PATTERN
+ *   TEST_MODEL
+
 
 **/
 def common = new com.mirantis.mk.Common()
 def gerrit = new com.mirantis.mk.Gerrit()
 
 node {
-    def cred = common.getCredentials(CREDENTIALS_ID, 'key')
-    def gerritChange = gerrit.getGerritChange(cred.username, GERRIT_HOST, GERRIT_CHANGE_NUMBER, CREDENTIALS_ID, true)
+    try {
 
-    stage('Trigger deploy job') {
-        build(job: STACK_DEPLOY_JOB, parameters: [
-            [$class: 'StringParameterValue', name: 'OPENSTACK_API_PROJECT', value: 'mcp-oscore'],
-            [$class: 'StringParameterValue', name: 'STACK_TEST', value: ''],
-            [$class: 'BooleanParameterValue', name: 'TEST_DOCKER_INSTALL', value: false]
-        ])
+        if (GERRIT_CHECK == true) {
+            def cred = common.getCredentials(CREDENTIALS_ID, 'key')
+            def gerritChange = gerrit.getGerritChange(cred.username, GERRIT_HOST, GERRIT_CHANGE_NUMBER, CREDENTIALS_ID, true)
+            def testrail = false
+        } else {
+            def testrail = TESTRAIL.toBoolean()
+        }
+
+        stage('Trigger deploy job') {
+            deployBuild = build(job: STACK_DEPLOY_JOB, parameters: [
+                [$class: 'StringParameterValue', name: 'OPENSTACK_API_PROJECT', value: 'mcp-oscore'],
+                [$class: 'StringParameterValue', name: 'STACK_INSTALL', value: 'core,openstack,ovs'],
+                [$class: 'StringParameterValue', name: 'STACK_TEST', value: ''],
+                [$class: 'StringParameterValue', name: 'STACK_TYPE', value: STACK_TYPE],
+                [$class: 'BooleanParameterValue', name: 'STACK_DELETE', value: false],
+            ])
+        }
+
+        // get SALT_MASTER_URL
+        def deployBuildParams = deployBuild.description.tokenize( ' ' )
+        SALT_MASTER_URL = "http://${deployBuildParams[1]}:6969"
+        STACK_NAME = "${deployBuildParams[0]}"
+        echo "Salt API is accessible via ${SALT_MASTER_URL}"
+
+        if (GERRIT_CHECK == true) {
+            stage('Run Smoke tests') {
+                build(job: STACK_TEST_JOB, parameters: [
+                    [$class: 'StringParameterValue', name: 'SALT_MASTER_URL', value: SALT_MASTER_URL],
+                    [$class: 'StringParameterValue', name: 'SALT_MASTER_CREDENTIALS', value: SALT_MASTER_CREDENTIALS],
+                    [$class: 'StringParameterValue', name: 'TEST_TEMPEST_TARGET', value: TEST_TEMPEST_TARGET],
+                    [$class: 'StringParameterValue', name: 'TEST_TEMPEST_PATTERN', value: 'set=smoke'],
+                    [$class: 'BooleanParameterValue', name: 'TESTRAIL', value: false],
+                    [$class: 'StringParameterValue', name: 'COMPONENT', value: 'smoke']
+                ])
+            }
+        }
+
+        stage("Run ${COMPONENT} tests") {
+            build(job: STACK_TEST_JOB, parameters: [
+                [$class: 'StringParameterValue', name: 'SALT_MASTER_URL', value: SALT_MASTER_URL],
+                [$class: 'StringParameterValue', name: 'SALT_MASTER_CREDENTIALS', value: SALT_MASTER_CREDENTIALS],
+                [$class: 'StringParameterValue', name: 'TEST_TEMPEST_TARGET', value: TEST_TEMPEST_TARGET],
+                [$class: 'StringParameterValue', name: 'TEST_TEMPEST_PATTERN', value: TEST_TEMPEST_PATTERN],
+                [$class: 'StringParameterValue', name: 'TEST_MILESTONE', value: MILESTONE],
+                [$class: 'StringParameterValue', name: 'TEST_MODEL', value: TEST_MODEL],
+                [$class: 'StringParameterValue', name: 'OPENSTACK_VERSION', value: OPENSTACK_VERSION],
+                [$class: 'BooleanParameterValue', name: 'TESTRAIL', value: testrail],
+                [$class: 'StringParameterValue', name: 'COMPONENT', value: COMPONENT]
+            ])
+        }
+    } catch (Throwable e) {
+        currentBuild.result = 'FAILURE'
+        throw e
+    } finally {
+
+        //
+        // Clean
+        //
+        if (common.validInputParam('STACK_DELETE') && STACK_DELETE.toBoolean() == true) {
+            stage('Trigger cleanup job') {
+                common.errorMsg('Stack cleanup job triggered')
+                build(job: STACK_CLEANUP_JOB, parameters: [
+                    [$class: 'StringParameterValue', name: 'STACK_NAME', value: STACK_NAME],
+                    [$class: 'StringParameterValue', name: 'STACK_TYPE', value: STACK_TYPE],
+                    [$class: 'StringParameterValue', name: 'OPENSTACK_API_URL', value: OPENSTACK_API_URL],
+                    [$class: 'StringParameterValue', name: 'OPENSTACK_API_CREDENTIALS', value: OPENSTACK_API_CREDENTIALS],
+                    [$class: 'StringParameterValue', name: 'OPENSTACK_API_PROJECT', value: OPENSTACK_API_PROJECT],
+                    [$class: 'StringParameterValue', name: 'OPENSTACK_API_PROJECT_DOMAIN', value: OPENSTACK_API_PROJECT_DOMAIN],
+                    [$class: 'StringParameterValue', name: 'OPENSTACK_API_PROJECT_ID', value: OPENSTACK_API_PROJECT_ID],
+                    [$class: 'StringParameterValue', name: 'OPENSTACK_API_USER_DOMAIN', value: OPENSTACK_API_USER_DOMAIN],
+                    [$class: 'StringParameterValue', name: 'OPENSTACK_API_CLIENT', value: OPENSTACK_API_CLIENT],
+                    [$class: 'StringParameterValue', name: 'OPENSTACK_API_VERSION', value: OPENSTACK_API_VERSION]
+                ])
+            }
+        }
     }
 }
