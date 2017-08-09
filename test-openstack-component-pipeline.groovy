c2b8221f8f119ac048dd71fb3e5442299795c228
Mykyta Karpin <mkarpin@mirantis.com>
2017-08-09 14:17:52 +0300

    [WIP] Add results upload and check
    Change-Id: I24be70d9234133dd1cb8c08d74deed3bd562f7ed
    Depends-On: Ic15002fb9eb97902c4abd124b46090f9c39e7ed5
    Related-Prod: OSCORE-270

--- test-openstack-component-pipeline.groovy
+++ test-openstack-component-pipeline.groovy
@@ -17,12 +17,65 @@ def gerrit = new com.mirantis.mk.Gerrit()
 node {
     def cred = common.getCredentials(CREDENTIALS_ID, 'key')
     def gerritChange = gerrit.getGerritChange(cred.username, GERRIT_HOST, GERRIT_CHANGE_NUMBER, CREDENTIALS_ID, true)
+    def deployJobName = STACK_DEPLOY_JOB
+    def deployBuild
+    def deployBuildNumber
+    def testOutputDir
 
     stage('Trigger deploy job') {
-        build(job: STACK_DEPLOY_JOB, parameters: [
+        deployBuild = build(job: "${deployJobName}", parameters: [
             [$class: 'StringParameterValue', name: 'OPENSTACK_API_PROJECT', value: 'mcp-oscore'],
             [$class: 'StringParameterValue', name: 'STACK_TEST', value: ''],
             [$class: 'BooleanParameterValue', name: 'TEST_DOCKER_INSTALL', value: false]
         ])
+    }
+
+    deployBuildNumber = deployBuild.getNumber()
+    testOutputDir = "output_${deployJobName}-${deployBuildNumber}"
+    sh("mkdir -p ${testOutputDir}")
+
+    stage('Get tests artifacts') {
+        selector = [$class: 'SpecificBuildSelector', buildNumber: deployBuildNumber];
+
+        step ([$class: 'CopyArtifact',
+               projectName: deployJobName,
+               selector: selector,
+               filter: '_artifacts/rally_reports.tar',
+               target: testOutputDir,
+               flatten: true]);
+
+        dir(testOutputDir) {
+            sh("tar -xf rally_reports.tar")
+        }
+    }
+
+    report = sh(script: "find ${testOutputDir} -name *.xml", returnStdout: true).trim()
+    reportPath = "${WORKSPACE}/${report}"
+
+    if (TESTRAIL.toBoolean() == true) {
+        stage('Upload tests results to Testrail'){
+
+            uploadResultsTestrail(reportPath, TESTRAIL_REPORTER_IMAGE, TEST_GROUP, TESTRAIL_QA_CREDS,
+                TEST_PLAN, TEST_MILESTONE, TEST_SUITE)
+        }
+    }
+
+    stage('Check tests results'){
+        fileContents = new File(reportPath)
+        parsed = new XmlParser().parse(fileContents)
+        res = parsed['testsuite'][0].attributes()
+        failures = res.failures.toInteger()
+
+        test_info = """
+                    Failed:  ${failures}
+                    Errors:  ${res.errors}
+                    Skipped: ${res.skipped}
+                    Tests:   ${res.tests}
+                    """
+        println test_info
+
+        if (failures > 0){
+            error("${failures} TEMPEST TESTS HAVE FAILED")
+        }
     }
 }
